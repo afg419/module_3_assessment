56e020c82d155d1a08748b2843d67a64c18d80fa
Aaron Greenspan <afg419@gmail.com>
2016-03-08 11:03:32 -0700

    Wondering how to do the last part of part 2

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
-    bbs = BestBuyService.new
-    reply = bbs.search(params["search"])
+    bbs = BestBuyService.new    
+      multi = true if params["search"].split.count > 1
+    reply = bbs.search(params["search"].split.first)
     @best_buy_objects = BestBuyPresenter.new(reply)
   end
 end

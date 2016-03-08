acae900a3c902b014fa3a6de7c814dff2c92ff59
Aaron Greenspan <afg419@gmail.com>
2016-03-08 10:18:27 -0700

    Creating Best Buy Service class, tricky cause the formatting of the requests is pretty elaborate

---
+++ app/models/best_buy_service.rb
@@ -0,0 +1,30 @@
+class BestBuyService
+
+  attr_reader :key, :base_url
+
+  def initialize
+    @key = "apiKey=#{ENV["BEST_BUY_KEY"]}"
+    @base_url = "https://api.bestbuy.com/v1/products"
+  end
+
+  def get(path, params = "")
+    params = "&" + params if !params.empty?
+    path = "(#{path})" if !path.empty?
+
+    uri = URI("#{base_url}#{path}?#{key}#{params}")
+    req = Net::HTTP::Get.new(uri)
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") {|http|
+      http.request(req)
+    }
+    res.body
+  end
+
+end
+
+  # products(search=sennheiser)
+  # url + search=sennheiser + "format=json&show=sku,name,salePrice,image"
+
+  "https://api.bestbuy.com/v1/products(search=oven&search=stainless&search=steel)?format=json&show=sku,name,salePrice&apiKey=YourAPIKey"
+  # get("products/8880044.json", "show=sku,name,salePrice")
+  # "https://api.bestbuy.com/v1/products/8880044.json?show=sku,name,salePrice&apiKey=YourAPIKeyend"
+# req.body = "grant_type=authorization_code&code=#{code}&redirect_uri=http://localhost:3000/reddit/auth"

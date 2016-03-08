ebafeddfc28797964eb4c30655380111541a62be
Aaron Greenspan <afg419@gmail.com>
2016-03-08 09:55:39 -0700

    Oops forgot to switch branches

--- spec/features/big_feature_spec.rb
+++ spec/features/big_feature_spec.rb
@@ -1,7 +1,10 @@
 RSpec.feature "User can search BestBuy", type: :feature do
   scenario "A user submit search queries to BestBuy" do
     VCR.use_cassette 'Best-Buy-Search' do
-      
+      visit root_path
+      fill_in "#search", with: "sennheiser"
+      click_on "Search"
+      expect(current_path).to eq "/search"
     end
   end
 end

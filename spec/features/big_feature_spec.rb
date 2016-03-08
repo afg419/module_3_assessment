7fad117d843ab65fbdf8cae17ae340705c7f7ab9
Aaron Greenspan <afg419@gmail.com>
2016-03-08 11:30:59 -0700

    Handles the final part

--- spec/features/big_feature_spec.rb
+++ spec/features/big_feature_spec.rb
@@ -23,9 +23,9 @@ RSpec.feature "User can search BestBuy", type: :feature do
   end
 
   scenario "A user submits multiple search queries to BestBuy" do
-    VCR.use_cassette 'Best-Buy-Multi-Search3' do
+    VCR.use_cassette 'Best-Buy-Multi-Search4' do
       visit root_path
-      fill_in "search", with: "sennheiser headphones"
+      fill_in "search", with: "sennheiser headphones white"
       click_on "Search"
       save_and_open_page
       expect(page).to have_selector('.product', count: 6)
@@ -35,11 +35,11 @@ RSpec.feature "User can search BestBuy", type: :feature do
       expect(page).to have_selector('.customer-review', count: 6)
       expect(page).to have_selector('.descritption', count: 6)
 
-      within(".sku-4763330") do
-        expect(page).to have_content "Galaxy Audio - Headset Microphone - Beige"
-        expect(page).to have_content "Sale Price: $199.99"
+      within(".sku-9068191") do
+        expect(page).to have_content "Sennheiser - CX 1.00 Earbud Headphones - White"
+        expect(page).to have_content "Sale Price: $44.95"
         expect(page).to have_content "Customer Review Average:"
-        expect(page).to have_content "Description: GALAXY AUDIO Headset Microphone: Crafted for most wireless transmitters; can be used with a JIB/PB; works with most PC sound cards; flexible fit design"
+        expect(page).to have_content "Description: SENNHEISER CX 1.00 Earbud Headphones: Noise-blocking design; dynamic transducer principle; ultrasmall design; 3.5mm connector; 3.9' cable; includes 4 sizes of ear adapters"
       end
       expect(current_path).to eq "/search"
     end

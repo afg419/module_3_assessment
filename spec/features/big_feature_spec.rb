cacd6e816e34ec2aff9b8c514506aba7a77e3e25
Aaron Greenspan <afg419@gmail.com>
2016-03-08 10:52:07 -0700

    Finishes first requirement of second part

--- spec/features/big_feature_spec.rb
+++ spec/features/big_feature_spec.rb
@@ -6,7 +6,18 @@ RSpec.feature "User can search BestBuy", type: :feature do
       click_on "Search"
 
       expect(page).to have_selector('.product', count: 15)
+      expect(page).to have_selector('.sku', count: 15)
+      expect(page).to have_selector('.sale-price', count: 15)
+      expect(page).to have_selector('.image-url', count: 15)
+      expect(page).to have_selector('.customer-review', count: 15)
+      expect(page).to have_selector('.descritption', count: 15)
 
+      within(".sku-4763330") do
+        expect(page).to have_content "Galaxy Audio - Headset Microphone - Beige"
+        expect(page).to have_content "Sale Price: $199.99"
+        expect(page).to have_content "Customer Review Average:"
+        expect(page).to have_content "Description: GALAXY AUDIO Headset Microphone: Crafted for most wireless transmitters; can be used with a JIB/PB; works with most PC sound cards; flexible fit design"
+      end
       expect(current_path).to eq "/search"
     end
   end

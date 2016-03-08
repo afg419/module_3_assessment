fce7c05ae446dcf3d75e3a7024baa1ea4e1b6bd2
Aaron Greenspan <afg419@gmail.com>
2016-03-08 11:49:31 -0700

    Tests basic best buy service call

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  # before(:each) do
-  # end
-
   it "successfully renders items" do
     get :index
     expect(response.status).to eq 200
--- spec/poros/best_buy_service_spec.rb
+++ spec/poros/best_buy_service_spec.rb
@@ -1,8 +1,33 @@
-describe "Best buy service does what it does" do
+require 'spec_helper'
+require 'rails_helper'
+#
+# RSpec.describe BestBuyService, type: :model do
+#
+#   VCR.use_casette "bbs-basic" do
+#     it "makes request to best buy" do
+#         bbs = BestBuyService.new
+#         reply = bbs.search("boot")
+#         binding.pry
+#     end
+#   end
+#
+# end
 
-  it "make requests to best buy" do
-
+RSpec.feature "BestBuy service does its thing", type: :feature do
+  scenario "A user submit search queries to BestBuy" do
+    VCR.use_cassette 'bbs-basic' do
+      bbs = BestBuyService.new
+      reply = bbs.search("boot")
+      expect(reply.except("products")).to eq ({"from"=>1,
+                                               "to"=>15,
+                                               "total"=>828,
+                                               "currentPage"=>1,
+                                               "totalPages"=>56,
+                                               "queryTime"=>"0.005",
+                                               "totalTime"=>"0.048",
+                                               "partial"=>false,
+                                               "canonicalUrl"=>
+                                               "/v1/products(search=\"boot\")?show=sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&apiKey=m7sfn4nqtmg8u96aakbfz49e"})
+    end
   end
-
-
 end

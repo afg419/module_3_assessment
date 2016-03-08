a413e7508a6f6756ebb46395e21d875e03b4e2ca
Aaron Greenspan <afg419@gmail.com>
2016-03-08 11:52:37 -0700

    Tests best buy service multi

--- spec/poros/best_buy_service_spec.rb
+++ spec/poros/best_buy_service_spec.rb
@@ -1,20 +1,5 @@
-require 'spec_helper'
-require 'rails_helper'
-#
-# RSpec.describe BestBuyService, type: :model do
-#
-#   VCR.use_casette "bbs-basic" do
-#     it "makes request to best buy" do
-#         bbs = BestBuyService.new
-#         reply = bbs.search("boot")
-#         binding.pry
-#     end
-#   end
-#
-# end
-
 RSpec.feature "BestBuy service does its thing", type: :feature do
-  scenario "A user submit search queries to BestBuy" do
+  scenario "BestBuyService Basics" do
     VCR.use_cassette 'bbs-basic' do
       bbs = BestBuyService.new
       reply = bbs.search("boot")
@@ -28,6 +13,23 @@ RSpec.feature "BestBuy service does its thing", type: :feature do
                                                "partial"=>false,
                                                "canonicalUrl"=>
                                                "/v1/products(search=\"boot\")?show=sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&apiKey=m7sfn4nqtmg8u96aakbfz49e"})
+    end
+  end
+
+  scenario "BestBuyService Multis" do
+    VCR.use_cassette 'bbs-multi' do
+      bbs = BestBuyService.new
+      reply = bbs.multi_search(["boot","brown"])
+      expect(reply.except("products")).to eq ({"from"=>1,
+                                               "to"=>2,
+                                               "total"=>2,
+                                               "currentPage"=>1,
+                                               "totalPages"=>1,
+                                               "queryTime"=>"0.021",
+                                               "totalTime"=>"0.029",
+                                               "partial"=>false,
+                                               "canonicalUrl"=>
+                                                "/v1/products(longDescription=\"boot*\"&longDescription=\"brown*\")?show=longDescription,sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&apiKey=m7sfn4nqtmg8u96aakbfz49e"})
     end
   end
 end
